729a03c1aa3d1f83b27a84e5c6e8c236b7a71c3e
aliena-mind <110843653+aliena-mind@users.noreply.github.com>
2023-02-04 18:58:42 -0400

    Correction d'erreur console
    - Utilisation de createRoot au lieu de render

--- kaza-app/src/index.jsx
+++ kaza-app/src/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react' // importation du module React
-import ReactDOM from 'react-dom' // importation du module ReactDOM
+import ReactDOM from 'react-dom/client' // importation du module ReactDOM
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom' // importation du router et de(s) route(s)
 
-// importation des differents éléments du site :
+// importation des différents éléments du site :
 import Header from './layout/Header'
 import Home from './pages/Home' 
 import About from './pages/About'
 import Error from './components/Error'
 
 // affichage des éléments :
-ReactDOM.render( 
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Header /> 
@@ -20,6 +20,5 @@ ReactDOM.render(
         <Route path="*" element={<Error/>} />
       </Routes>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
