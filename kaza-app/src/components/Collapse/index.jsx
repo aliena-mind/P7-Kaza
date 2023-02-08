ccba8948c6b9b9038d85d00e101364f7d69d921f
aliena-mind <110843653+aliena-mind@users.noreply.github.com>
2023-02-07 22:06:50 -0400

    Création du component 'Collapse'

---
+++ kaza-app/src/components/Collapse/index.jsx
@@ -0,0 +1,31 @@
+import '../../styles/components/Collapse/index.css'
+import { useState } from 'react';
+
+const collapseOpen = require('../../assets/collapse/collapse-open.png');
+const collapseClose = require('../../assets/collapse/collapse-close.png');
+
+function Collapse(props) {
+
+    // définit l'état 'showContent' initialisé à 'false' (contenu masqué)
+    const [showContent, setShowContent] = useState(false); 
+
+    return (
+        <div className="collapse-container">
+            <button onClick={() => setShowContent(!showContent)}>  {/* inverse la valeur de 'showContent' lors du click */}
+                {props.text} {/* 'text' transmis via les "props" */}
+                {showContent ? (
+                    <img className='collapse-image' src={collapseOpen} alt="collapseOpen" /> // si 'showContent' est 'true', l'image est 'collapseOpen'
+                ) : (
+                    <img className='collapse-image' src={collapseClose} alt="collapseClose" /> // si 'showContent' est 'false', l'image est 'collapseClose'
+                )}
+            </button>
+            {showContent && ( // affiche le contenu de 'collapse-content' si 'showContent' est 'true'
+                <div className='collapse-content'> {/* ajout de la classe 'show-content' si 'showContent' est 'true'  */}
+                    Contenu à afficher ou masquer
+                </div>
+            )}
+        </div>
+    )
+}
+ 
+export default Collapse
--- kaza-app/src/pages/About/index.jsx
+++ kaza-app/src/pages/About/index.jsx
@@ -3,11 +3,17 @@ import '../../styles/pages/About/index.css'
 import Banner from '../../components/Banner'; // importation du composant Banner
 import banner from '../../assets/banner/banner-about.png' // importation de l'image de la bannière
 
+import Collapse from '../../components/Collapse'; // importation du composant Collapse
+
 function About() {
     return (
         <div className="about">
             <Banner image={banner} className="about-banner-container"/>
             <h1>À propos..</h1>
+            <Collapse text='Fiabilité' />
+            <Collapse text='Respect' />
+            <Collapse text='Service' />
+            <Collapse text='Responsabilité' />
         </div>
     )
 }
