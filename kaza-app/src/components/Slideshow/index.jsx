1f14b17ed7ba1c0d61d3f0cac926ebbc013cda28
aliena-mind <110843653+aliena-mind@users.noreply.github.com>
2023-02-09 17:45:08 -0400

    Création du composant 'Showslide'

---
+++ kaza-app/src/components/Slideshow/index.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import '../../styles/components/Slideshow/index.css'
+
+const slideshowPrevious = require('../../assets/slideshow/slideshow-previous.png');
+const slideshowNext = require('../../assets/slideshow/slideshow-next.png');
+
+function Slideshow(props) {
+
+    // définit l'état 'currentIndex' initialisé à '0' (index = 0)
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    // fonction fléchée appelée lorsque l'utilisateur clique sur le bouton 'Suivant'
+    const nextSlide = () => {
+        // si 'currentIndex + 1' >= 'props.pictures.length', 'currentIndex = 0', sinon 'currentIndex = currentIndex + 1'
+        setCurrentIndex(currentIndex + 1 >= props.pictures.length ? 0 : currentIndex + 1); 
+    };
+
+    // fonction fléchée appelée lorsque l'utilisateur clique sur le bouton 'Précédent'
+    const previousSlide = () => {
+        // si 'currentIndex - 1 < 0', 'currentIndex = props.pictures.length - 1', sinon 'currentIndex = currentIndex - 1'
+        setCurrentIndex(currentIndex - 1 < 0 ? props.pictures.length - 1 : currentIndex - 1);
+    };
+
+    return (
+        <div className="slideshow">
+            <div>
+                <ul>
+                    {/* pour chaque image de 'props.pictures' : */}
+                    {props.pictures.map((picture, index) => ( 
+                        // Si l'index est égal à la valeur de 'currentIndex', l'image correspondante sera affiché
+                        <li className='pictures-container' key={picture} style={{ display: index === currentIndex ? 'block' : 'none' }}> 
+                            <img className='slide-picture' src={picture} alt="Housing" />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className='button-container'>
+                {/* Si props.pictures.length > 1, alors les boutons seront affichés */}
+                {props.pictures.length > 1 && (
+                    <> {/* raccourci création d'un élément React vide, 'Fragment', les boutons seront ajouté à l'élément parent */}
+                        <button className="slide-button" onClick={previousSlide}> 
+                            <img className='slide-button-img' src={slideshowPrevious} alt="Précédent" />
+                        </button>
+                        <button className="slide-button" onClick={nextSlide}>
+                            <img className='slide-button-img' src={slideshowNext} alt="Suivant" />
+                        </button>
+                    </>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Slideshow
--- kaza-app/src/pages/House/index.jsx
+++ kaza-app/src/pages/House/index.jsx
@@ -1,15 +1,23 @@
 import { useParams } from "react-router-dom"
 import housingList from "../../datas/housingList" // importation des datas "housingList"
+import Slideshow from "../../components/Slideshow"
 
 
 function House() {
     const { id } = useParams(window.location.href) // récupère le paramètre id dans l'url
 
-    const houseElement = housingList.find((object) => object.id === id) // cherche l'objet de meme id disponible dans 'housingList'
+    const houseElement = housingList.find((house) => house.id === id) // cherche 'house' de meme id disponible dans 'housingList'
 
     return (
         <div>
-            <p> { houseElement.id } </p>
+            <Slideshow pictures= { houseElement.pictures }/> {/* props 'picture' de 'houseElement' */}
+            <h1> { houseElement.title } </h1> {/* props 'title' de 'houseElement' */}
+            <p> { houseElement.location }</p> {/* props 'location' de 'houseElement' */}
+            <div>
+                {houseElement.tags.map(tag => ( // pour chaque 'tag' parmi 'tags' comprise dans 'houseElement' :
+                    <span key={tag}>{tag}</span>
+                ))}
+            </div>
         </div>
     )
 }
